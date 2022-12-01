8489a26e78b5869dc0d6937e6550d4ddcc47897f
Andrea Rosentel <andrearosentel@gmail.com>
2022-12-01 14:26:49 -0800

    done with 01-01

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,13 +1,17 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let output = -1
+  if (arr.length > 1) {
+    for (i=0;i<arr.length;i++) {
+      arr[i] === arr[i+1] ? output = arr[i] : null
+    }
+  }
+  return output
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
-
-  console.log("");
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
@@ -1,15 +1,28 @@
+require 'byebug'
+
 def find_first_duplicate(arr)
-  # type your code in here
+  if arr.size > 1
+    i = 0
+    while i < arr.size
+      if arr[i] == arr[i+1]
+        return arr[i]
+      else
+        i = i + 1
+      end
+    end
+  end
+  -1
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 3"
   puts "=>", find_first_duplicate([2, 1, 3, 3, 2])
 
-  puts
-
   puts "Expecting: -1"
   puts "=>", find_first_duplicate([1, 2, 3, 4])
+
+  puts 'Expecting: 5'
+  puts '=>', find_first_duplicate([5, 5, 4, 2, 1])
 
   # Don't forget to add your own!
 end
